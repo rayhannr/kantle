57f9c261d5fe2869846298b5620f109765e034d2
rayhannr <rayhan@accelbyte.net>
2022-03-10 22:45:45 +0700

    feat: add twitter card meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,25 @@ const Home: NextPage = () => {
       <Head>
         <title>{GAME_TITLE}</title>
         <meta name="description" content={GAME_DESCRIPTION} />
+        <meta
+          name="keywords"
+          content="game, permainan, main, tebak, kata, rahasia, clue, petunjuk, wordle, bahasa, indonesia, kbbi, kantle"
+        />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={GAME_TITLE} />
+        <meta name="twitter:description" content={GAME_DESCRIPTION} />
+        <meta name="twitter:image" content="https://kantle.vercel.app/images/kantle.png" />
+
+        <meta property="og:title" content={GAME_TITLE} />
+        <meta property="og:description" content={GAME_DESCRIPTION} />
+        <meta property="og:image" content="https://kantle.vercel.app/images/kantle.png" />
+        <meta property="og:url" content="https://kantle.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:keywords"
+          content="game, permainan, main, tebak, kata, rahasia, clue, petunjuk, wordle, bahasa, indonesia, kbbi, kantle"
+        />
       </Head>
       <AlertProvider>
         <App />
